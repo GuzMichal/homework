e3dfbda5d73fb0d8637c9a6dff8bea20e16fca4d
GuzMichal <79877926+GuzMichal@users.noreply.github.com>
2021-04-09 00:14:04 +0200

    Homework - Week 5
    JavaScript

---
+++ JavaScript/jsTasks.js
@@ -0,0 +1,133 @@
+// Zad1
+let name = "Karol";
+let age = 40;
+let isOpen = true;
+let stringBoolean = true;
+let stringNumber = 100;
+// Zad2
+console.log("Michał");
+console.log("Hello Ania");
+console.log(4);
+console.log(false);
+// Zad3
+console.log(name);
+console.log(age);
+console.log(isOpen);
+console.log(stringBoolean);
+console.log(stringNumber);
+// Zad4
+console.log(2 + 2);
+console.log("Michał " + "Kowalski");
+console.log(2 + 2 * 2);
+// Zad5
+let firstNumber = 3;
+let secondNumber = 6;
+let add = firstNumber + secondNumber;
+console.log("wynik " + add);
+// Zad6
+let variable = "BMW";
+variable = "Volvo";
+variable = "Pagani";
+console.log(variable);
+// Zad7
+function sumOfTwo() {
+  return 4;
+}
+function hello() {
+  return "Hello World";
+}
+function boolean() {
+  return true;
+}
+function modulo() {
+  return 6 % 4;
+}
+function power() {
+  return 3 ** 4;
+}
+function hello() {
+  return "Hello," + " how are you?";
+}
+function equationOne() {
+  return 5 * 60;
+}
+function equationTwo() {
+  return 3 * 3600;
+}
+function equationThree() {
+  return (10 * 9) / 2;
+}
+function equationFour() {
+  return 200 - 10;
+}
+// Zad8
+let numbers = [1, 2, 3];
+let cars = ["BMW", "Volvo", "Lamborghini", "FSO", "Fiat"];
+let boolean = [true, false, true];
+let empty = [];
+let mix = ["BMW", 1, true, "FSO", false];
+console.log(mix[1]);
+fruits = ["Banan", "Mango", "Jabłko"];
+console.log(fruits.length);
+let secondEmpty = [];
+secondEmpty.push("turbo");
+console.log(secondEmpty);
+// Zad9
+const emptyObject = {};
+const person = {
+  name: "Iza",
+};
+const personDetails = {
+  name: "John",
+  lastName: "Rambo",
+  age: 45,
+  drivingLicence: true,
+};
+console.log(personDetails.age);
+console.log(personDetails);
+// Zad10
+function sum(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+function showInscription(inscription) {
+  return inscription;
+}
+function answer(correct) {
+  return true;
+}
+function modulo2(firstNumber, secondNumber) {
+  return firstNumber % secondNumber;
+}
+function power2(firstNumber, secondNumber) {
+  return firstNumber ** secondNumber;
+}
+function strings(firstString, secondString) {
+  return firstString + secondString;
+}
+function minutesToSeconds(minutes) {
+  return 60 * minutes;
+}
+function hoursToSeconds(hours) {
+  return 3600 * hours;
+}
+function areaOfTriangle(a, b) {
+  return a * b * 0.5;
+}
+function yearsToDays(years) {
+  return 365 * years;
+}
+function power3(number) {
+  return number ** number;
+}
+function triangleSide(side1, side2) {
+  return side1 + side2 - 1;
+}
+function hello2(name) {
+  return "Część, " + name;
+}
+function age(year) {
+  return 2021 - year;
+}
+function score(twoPoints, threePoints) {
+  return 2 * twoPoints + 3 * threePoints;
+}
